d2cc0414cebb377902c886dfed95c85600256b60
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-07-14 13:52:02 +0500

    fix bugs

--- login.js
+++ login.js
@@ -4,12 +4,12 @@ function gotohome() {
 
     var a = localStorage.getItem('userDetail')
     
-    if (email1.value === JSON.parse(a).email || JSON.parse(a).email === "" && password1.value === JSON.parse(a).password || JSON.parse(a).password === " ") {
+    if (email1.value === "" || password1.value === "") {
        
-        location.href = "home.html"
+        alert("fill both fields")
     }
-    else if(email1.value === "" || password1.value === ""){
-alert("fill both fields")
+    else if (email1.value === JSON.parse(a).email && password1.value === JSON.parse(a).password){
+        location.href = "home.html"
     }
      else {
         alert("wrong email or password")
