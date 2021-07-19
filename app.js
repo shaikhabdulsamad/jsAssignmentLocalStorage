607f664276e10711f48e1a5fe51f358ab2bb2638
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-07-19 10:59:09 +0500

    fixed bugs

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-function gotonext(){
-var name = document.getElementById("inputName")
-var number = document.getElementById("inputNumber")
-var email = document.getElementById("inputEmail")
-var password = document.getElementById("inputPassword")
+function gotonext() {
+    var name = document.getElementById("inputName")
+    var number = document.getElementById("inputNumber")
+    var email = document.getElementById("inputEmail")
+    var password = document.getElementById("inputPassword")
 
-if(email.value !== "" || password.value !== "" || name.value !== "" || number.value !== ""){
+    if (email.value !== "" || password.value !== "" || name.value !== "" || number.value !== "") {
 
-    if(localStorage.getItem('userDetail') === null){
-        var abc = [];
-        localStorage.setItem('userDetail', JSON.stringify(abc));  
+        if (localStorage.getItem('userDetail') === null) {
+            var abc = [];
+            localStorage.setItem('userDetail', JSON.stringify(abc));
+        }
+
+        var userDetail = {
+            name: name.value,
+            number: number.value,
+            email: email.value,
+            password: password.value
+        }
+
+
+        var get = JSON.parse(localStorage.getItem('userDetail'));
+        get.push(userDetail);
+
+        localStorage.setItem('userDetail', JSON.stringify(get));
+
+
+        // localStorage.setItem('userDetail',JSON.stringify(userDetail))
     }
 
-var userDetail = {
-    name: name.value,
-    number: number.value,
-    email: email.value,
-    password: password.value
-}
 
+    else {
+        alert('fill all fields')
 
-var get =  JSON.parse(localStorage.getItem('userDetail'));
-get.push(userDetail);
+    }
 
-localStorage.setItem('userDetail', JSON.stringify(get));
+    var a = JSON.parse(localStorage.getItem('userDetail'));
+    var flag = false;
 
+    for(var i = 0; i < a.length; i++){
 
-// localStorage.setItem('userDetail',JSON.stringify(userDetail))
-}
-
-
-else{
-    alert('fill all fields')
+        if(email.value === a[i].email || number.value === a[i].number){
+            flag = true;
+    alert("email or password already exist")
+    break
     
-}
+        }
+    }
 }
 
 
