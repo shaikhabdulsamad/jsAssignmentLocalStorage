02139b674feb7b5d9566e1bb04074d6aced6fdd1
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-07-19 11:40:20 +0500

    working to fix bugs

--- app.js
+++ app.js
@@ -1,56 +1,38 @@
-function gotonext() {
-    var name = document.getElementById("inputName")
-    var number = document.getElementById("inputNumber")
-    var email = document.getElementById("inputEmail")
-    var password = document.getElementById("inputPassword")
+function gotonext(){
+var name = document.getElementById("inputName")
+var number = document.getElementById("inputNumber")
+var email = document.getElementById("inputEmail")
+var password = document.getElementById("inputPassword")
 
-    var a = JSON.parse(localStorage.getItem('userDetail'));
-    var flag = false;
+if(email.value !== "" || password.value !== "" || name.value !== "" || number.value !== ""){
 
-    for (var i = 0; i < a.length; i++) {
-
-        if (email.value === a[i].email || number.value === a[i].number) {
-            flag = true;
-            alert("email or password already exist")
-            break
-
-        }
-    }
-if(flag == false){
-
-
-
-    if (email.value !== "" || password.value !== "" || name.value !== "" || number.value !== "") {
-
-        if (localStorage.getItem('userDetail') === null) {
-            var abc = [];
-            localStorage.setItem('userDetail', JSON.stringify(abc));
-        }
-
-        var userDetail = {
-            name: name.value,
-            number: number.value,
-            email: email.value,
-            password: password.value
-        }
-
-
-        var get = JSON.parse(localStorage.getItem('userDetail'));
-        get.push(userDetail);
-
-        localStorage.setItem('userDetail', JSON.stringify(get));
-
-
-        // localStorage.setItem('userDetail',JSON.stringify(userDetail))
+    if(localStorage.getItem('userDetail') === null){
+        var abc = [];
+        localStorage.setItem('userDetail', JSON.stringify(abc));  
     }
 
-
-    else {
-        alert('fill all fields')
-
-    }
+var userDetail = {
+    name: name.value,
+    number: number.value,
+    email: email.value,
+    password: password.value
 }
-   
+
+
+var get =  JSON.parse(localStorage.getItem('userDetail'));
+get.push(userDetail);
+
+localStorage.setItem('userDetail', JSON.stringify(get));
+
+
+// localStorage.setItem('userDetail',JSON.stringify(userDetail))
+}
+
+
+else{
+    alert('fill all fields')
+    
+}
 }
 
 
